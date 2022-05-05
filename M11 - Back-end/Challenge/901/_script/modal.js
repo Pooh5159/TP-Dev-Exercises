78f744d640ae33ee072288ea0b8c72bb32369b74
Pooh5159 <Pierson.Pauline@gmail.com>
2022-05-06 00:04:27 +0200

    final version

--- M11 - Back-end/Challenge/901/_script/modal.js
+++ M11 - Back-end/Challenge/901/_script/modal.js
@@ -20,3 +20,32 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function () {
     modal.style.display = "none";
 }
+
+
+// Status boolean used to determine if the modal zoom in animation is finished
+// Initialized to false
+let modalZoomedIn = false;
+
+// Add an eventListener on the whole document to handle click events
+document.addEventListener("click", function (e) {
+
+    // Find the closest element that matches the CSS selector "modal-content"
+    // If our example if we click the modal image we have "<img id="img" class="modal-content" src="https://www.thesportsdb.…sey/fmwt4p1599922694.png">" in the console"
+    const clickedElement = e.target.closest(".modal-content");
+
+    // If not clickedElement means we clicked on something other than the image
+    // We also need to make sure the modal zoom in animation is over
+    // If modal animation is done and user clicks outside of the image in the modal
+    if (modalZoomedIn && !clickedElement) {
+        // We close the modal and reset the boolean
+        modal.style.display = "none";
+        modalZoomedIn = false;
+
+    }
+
+});
+
+// This event listener will be triggered when captionText zoom in animation is completed
+captionText.addEventListener("animationend", () => {
+    modalZoomedIn = true;
+})
